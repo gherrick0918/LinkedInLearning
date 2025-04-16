72441842fbc458ca0ead95ac8fcd8785f562299a
Gregory Herrick <gherrick0918@gmail.com>
2025-04-16 09:02:31 -0400

    My work on LinkedIn Learning Python course, Chapter 2 - Basics

---
+++ learning-python-2896241-main/Ch2 - Basics/modules_start.py
@@ -0,0 +1,19 @@
+# LinkedIn Learning Python course by Joe Marini
+#
+
+
+# TODO: import the math module, which contains features for working with mathematics
+import math
+
+
+# TODO: the math module contains lots of pre-built functions
+print("The square root of 16 is ", math.sqrt(16))
+
+# TODO: in addition to functions, some modules contain useful constants 
+print("Pi is", math.pi)
+
+# TODO: try some of the math functions for yourself here:
+print(math.ceil(1.2))
+print(math.floor(1.2))
+print(math.pow(2, 3))
+print(math.factorial(5))
